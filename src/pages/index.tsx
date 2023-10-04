4b0555919becfaab376a9c27f50265d265af8662
Julien Béranger <julien@strat.cc>
2023-10-04 20:49:14 +0200

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,109 @@
+import { Text, Button, useToast } from '@chakra-ui/react'
+import { Head } from 'components/layout/Head'
+import { HeadingComponent } from 'components/layout/HeadingComponent'
+import { LinkComponent } from 'components/layout/LinkComponent'
+import { useState, useEffect } from 'react'
+import { useAccount, useNetwork, useWalletClient, useSwitchNetwork } from 'wagmi'
+import { ethers } from 'ethers'
+import { NFT_CONTRACT_ADDRESS, NFT_CONTRACT_ABI } from '../utils/nft'
+import { walletClientToSigner } from '../hooks/ethersAdapter'
+
+export default function Home() {
+  const { isConnected } = useAccount()
+  const { data: walletClient, isError } = useWalletClient()
+  const toast = useToast()
+  const { chain } = useNetwork()
+  const { chains, error, isLoading, pendingChainId, switchNetwork } = useSwitchNetwork()
+
+  const [loading, setLoading] = useState<boolean>(false)
+  const [signer, setSigner] = useState<any>()
+  const [txLink, setTxLink] = useState<string>()
+  const [txHash, setTxHash] = useState<string>()
+
+  useEffect(() => {
+    const init = async () => {
+      console.log('isConnected:', isConnected)
+      if (walletClient) {
+        setSigner(walletClientToSigner(walletClient))
+      }
+      if (chain?.id !== 10243) {
+        switchNetwork?.(10243)
+      }
+    }
+    init()
+  }, [walletClient])
+
+  const mint = async () => {
+    try {
+      if (!walletClient) {
+        toast({
+          title: 'No wallet',
+          description: 'Please connect your wallet first.',
+          status: 'error',
+          position: 'bottom',
+          variant: 'subtle',
+          duration: 9000,
+          isClosable: true,
+        })
+        return
+      }
+      setLoading(true)
+      setTxHash('')
+      setTxLink('')
+      const nft = new ethers.Contract(NFT_CONTRACT_ADDRESS, NFT_CONTRACT_ABI, signer)
+      const call = await nft.safeMint(signer.address)
+      const receipt = await call.wait()
+      console.log('tx:', receipt)
+      setTxHash(receipt.hash)
+      setTxLink('https://explorer-test.arthera.net/tx/' + receipt.hash)
+      setLoading(false)
+      toast({
+        title: 'Successful mint',
+        description: 'Congrats, your NFT was minted! 🎉',
+        status: 'success',
+        position: 'bottom',
+        variant: 'subtle',
+        duration: 20000,
+        isClosable: true,
+      })
+    } catch (e) {
+      setLoading(false)
+      console.log('error:', e)
+      toast({
+        title: 'Woops',
+        description: 'Something went wrong during the minting process...',
+        status: 'error',
+        position: 'bottom',
+        variant: 'subtle',
+        duration: 9000,
+        isClosable: true,
+      })
+    }
+  }
+
+  return (
+    <>
+      <Head />
+
+      <main>
+        <HeadingComponent as="h2">Hi there! 👋</HeadingComponent>
+        <Button
+          mt={4}
+          colorScheme="blue"
+          variant="outline"
+          type="submit"
+          onClick={mint}
+          isLoading={isLoading}
+          loadingText="Minting..."
+          spinnerPlacement="end">
+          Mint
+        </Button>
+        {txHash && (
+          <Text py={4} fontSize="14px" color="#45a2f8">
+            <LinkComponent href={txLink ? txLink : ''}>{txHash}</LinkComponent>
+          </Text>
+        )}
+      </main>
+    </>
+  )
+}
